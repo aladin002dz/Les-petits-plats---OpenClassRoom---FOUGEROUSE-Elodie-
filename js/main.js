03ce07aad7d9e09c44a7f330024c8ae816923388
aladin002dz <mahfoudh.arous@gmail.com>
2026-03-17 18:16:19 +0100

    ♻️ refactort: mettre le json des recettes dans un dossier data

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ import { initFilters } from "./filters.js";
 
 async function init() {
   // Charge les recettes depuis recipes.json
-  const url = new URL("./recipes.json", import.meta.url).href;
-  const res = await fetch(url);
+  //const url = new URL("./data/recipes.json", import.meta.url).href;
+  const res = await fetch("./data/recipes.json");
   if (!res.ok) return;
   const recipes = await res.json();
   if (!Array.isArray(recipes)) return;
